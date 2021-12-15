de52da4b178f3bd479b02df852653843d65634e6
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2021-12-15 13:56:25 +0700

    init package

--- database/migrations/2021_12_15_062636_create_barang.php
+++ database/migrations/2021_12_15_062636_create_barang.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBarang extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('barangs', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->string('roles');
             $table->string('nama');
-            $table->text('alamat');
-            $table->string('no_hp');
+            $table->integer('harga');
+            $table->integer('qty');
+            $table->string('satuan');
+            $table->text('gambar');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('barangs');
     }
 }
---
+++ database/migrations/2021_12_15_063034_create_transaksi.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksi extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->date('tanggal');
+            $table->string('no_transaksi');
+            $table->integer('sub_total');
+            $table->integer('ongkir');
+            $table->integer('total');
+            $table->string('ekspedisi');
+            $table->text('alamat');
+            $table->smallInteger('status_transaksi');
+            $table->smallInteger('status_pembayaran');
+            $table->text('url');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksis');
+    }
+}
