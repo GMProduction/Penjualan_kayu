f4fafe6e4c7728d6729e2ba9566c7ee4d4a8f58a
pradana <pradanamahendra@gmail.com>
2021-12-29 11:49:53 +0700

    asd

--- resources/views/admin/surat.blade.php
+++ resources/views/admin/surat.blade.php
@@ -75,7 +75,7 @@
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Tambah Siswa</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Tambah Admin</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                 aria-label="Close"></button>
                         </div>
